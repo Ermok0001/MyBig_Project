2da1cd01c2f2fd085c34d21fbf9bb0bd7417f898
Your N <you@example.com>
2024-02-20 15:58:35 +0600

    big_project

--- src/components/Basic/Navigation.jsx
+++ src/components/Basic/Navigation.jsx
@@ -1,59 +1,3 @@
-// import { Link } from "react-router-dom";
-// import menu_img from "../assets/menu.png";
-// import search_img from "../assets/search.png";
-// import person_img from "../assets/person_outline.png";
-// import { useState } from "react";
-// import { SideBar } from "../other_components/SideBar";
-// import { useDispatch } from "react-redux";
-// import { addToSortedContent } from "../../store/reducer";
-
-// export function Navigation() {
-//     const dispatch = useDispatch
-//     const [sideBar, setSideBar] = useState(false);
-//     const [searchTerm, setSearchTerm] = useState('')
-
-//     function handleSearch(){
-//         const sortedArray = direction.filter((item) => item.title.includes(inputValue))
-//         dispatch( addToSortedContent (sortedArray))
-//     }
-
-//     return (
-//         <div className="All_Navigation">
-//             <div className="Navigation">
-//                 <div className="Info_Navigation">
-//                         <img style={{ cursor: 'pointer' }} onClick={()=>{setSideBar(!sideBar)}} src={menu_img} alt="Menu" />
-//                         {sideBar ? (
-//                             <div className="Sidebar">
-//                                 <SideBar
-//                                 setSideBar = {setSideBar}>
-//                                 </SideBar>
-//                             </div>
-//                         ) : (
-//                             <div>
-//                                 <h2>Erasyl Hotels</h2>
-//                             </div>
-//                         )}
-//                     <Link to='/'><p>Main</p></Link>
-//                     <Link to="/fav"><p>Favourite</p></Link>
-//                     <Link to="/about"><p>About</p></Link>
-//                     <Link to="/support"><p>Support</p></Link>
-//                 </div>
-//                 <div className="Search_Navigation">
-//                     <img style={{ cursor: 'pointer' }} onClick={()=>{handleSearch}} src={search_img} alt="Search" />
-//                     <input 
-//                     type="text" 
-//                     placeholder="Search"
-//                     value={searchTerm}
-//                     onChange={(e)=>{setSearchTerm(e.target.value)}}/>
-//                     <img style={{ cursor: 'pointer' }} src={person_img} alt="Person" />
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
-//////////////////////////////////////////////////////////////////////////////
-
 import { Link } from "react-router-dom";
 import menu_img from "../assets/menu.png";
 import search_img from "../assets/search.png";
@@ -68,13 +12,16 @@ export function Navigation() {
     const dispatch = useDispatch();
     const [sideBar, setSideBar] = useState(false);
     const [searchTerm, setSearchTerm] = useState('');
-    const{ data } = useHotelsQuery
+    const{ data: direction } = useHotelsQuery
 
-    // function handleSearch() {
-    //     const sortedArray = data.filter((item) => item.title.includes(searchTerm));
-    //     dispatch(addToSortedContent(sortedArray));
-    // }
+    function handleSearch() {
+        if (direction) {
+            const sortedArray = direction.filter((item) => item.title.includes(searchTerm));
+            dispatch(addToSortedContent(sortedArray));
+        }
+    }
 
+    console.log(searchTerm)
     return (
         <div className="All_Navigation">
             <div className="Navigation">
@@ -95,7 +42,7 @@ export function Navigation() {
                     <Link to="/support"><p>Support</p></Link>
                 </div>
                 <div className="Search_Navigation">
-                    {/* <img style={{ cursor: 'pointer' }} onClick={handleSearch} src={search_img} alt="Search" /> */}
+                    <img style={{ cursor: 'pointer' }} onClick={handleSearch} src={search_img} alt="Search" />
                     <input 
                         type="text" 
                         placeholder="Search"
--- src/components/page/Favourite.jsx
+++ src/components/page/Favourite.jsx
@@ -1,5 +1,5 @@
 import {useSelector } from "react-redux"
-// import { addTofav, removeFromfav } from "../../store/reducer"
+import { addTofav, removeFromfav } from "../../store/reducer"
 
 export function Favoutite({direction}){
     const favState = useSelector((state)=> state.fav.fav)
@@ -8,7 +8,7 @@ export function Favoutite({direction}){
     return(
         <div className="All_fav">
             <h1>favourite</h1>
-            <h1>{direction.country}</h1>
+            <h1>{direction}</h1>
         </div>
     )
 }
